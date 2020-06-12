2260518c6f4bcdf9d70b8183745f60594a2dce64
247076126@qq.com <247076126@qq.com>
2020-06-12 20:09:52 +0800

    react router 404

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React from 'react';
 import Header from './common/header';
 import store from './store/index';
 import { Provider } from 'react-redux';
-import { BrowserRouter,Route } from "react-router-dom";
+import { BrowserRouter,Route,Switch } from "react-router-dom";
 import Hemo from './pages/home';
 import Detail from './pages/detail';
+import NoFind from './pages/noFind';
 
 function App() {
   return (
@@ -13,8 +14,11 @@ function App() {
         <BrowserRouter>
               {/* <Route path="/" exact render={ ()=> <div>hemo</div>}></Route>
               <Route path="/detail" exact render={ ()=> <div>detail</div>}></Route> */}
-              <Route path="/" exact component={Hemo}></Route>
-              <Route path="/detail" exact component={Detail}></Route>
+              <Switch>
+                <Route path="/" exact component={Hemo}></Route>
+                <Route path="/detail" exact component={Detail}></Route>
+                <Route component={NoFind}></Route>
+              </Switch>
         </BrowserRouter>
     </Provider> 
   );
---
+++ src/pages/noFind/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+class NoFind extends React.Component{
+    render(){
+        return (
+            <div>404 没有找到嘿嘿</div>
+        )
+    }
+}
+export default NoFind;
