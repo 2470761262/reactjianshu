d3f0c4ab7b37511799a888beb26ba5d5577640ad
247076126@qq.com <247076126@qq.com>
2020-06-13 23:06:47 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ class App extends React.Component {
     }
 
     handleClick = ()=>{  
-        //使用router的render 这样就有点像vue的component动态组件
+        //使用router的render 也可以动态组件
         this.setState(()=>{
             return {
                 gogo : <ul>
